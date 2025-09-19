71db1d6edd774efb3306698e6a8d8301a99dbff1
Joacsnk <Joacsnk@gmail.com>
2025-09-18 21:57:39 -0300

    Nova interface
    Adição de sleep e de lib questionary para interface.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from connection import conexao_DB
 from os import system as sy
-        
+from time import sleep as sl
+import questionary
+
 class Main():
     
     def __init__(self) -> None:
@@ -20,5 +22,36 @@ class Main():
             print("Sistema com problemas internos... ❌")
         else:
             print("\nSistema rodando... ✅")
-    
+        sl(3)
+        sy("cls")
+        self.selecionar_opcao()
+        
+    def interface(self):
+        print('"_.~"~._.~"~._.~"~._.~"~._.~"~._.~"~._.~"~._.~"~._\n')
+        print('                   BIRTHDAY APP                     ')
+        print('\n"_.~"~._.~"~._.~"~._.~"~._.~"~._.~"~._.~"~._.~"~._\n')
+        custom_style = questionary.Style([
+                ("instruction", "hidden"),
+        ])
+        opcao = questionary.select(
+            "",
+            choices=[">>------> Adicionar aniversário", ">>------> Excluir aniversário", 
+                     ">>------> Pesquisar aniversário", ">>------> Sair"],
+            instruction=None,
+            qmark="",
+            style=custom_style
+            
+        ).ask()
+        return opcao
+        
+    def selecionar_opcao(self):
+        match self.interface():
+            case ">>------> Sair":
+                sy("cls")
+                print("Programa encerrado com sucesso ✅")
+                exit()
+        
+            
+
+sl(3)
 Main().main()
